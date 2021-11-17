0bdd3da4d73960ff55df9b8704953af6106fdd70
Dovile Tumaite <dovile.tumaite@yahoo.it>
2021-11-17 12:23:49 +0100

    register works

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -3,8 +3,29 @@ import "../styles/login.css";
 import logo from "../styles/logo.png";
 import google from "../styles/google.png"
 import { Link } from "react-router-dom";
+import { useState } from "react";
+import API from "../tools/api";
 
 const Register = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [username, setUsername] = useState("");
+  
+  const login = async () => {
+    const { data } = await API.post(
+      "/user/account",
+      { username, email, password },
+      { method: "POST" }
+    );
+    console.log("data post req", data);
+    localStorage.setItem("accessToken", data.accessToken);
+    localStorage.setItem("refreshToken", data.refreshToken);
+  };
+  const getUserInfo = async () => {
+    const { data } = await API.get("/user/me");
+    console.log("me", data);
+  };
+
     return(
         <div>
         <Navbar className="navBar_login">
@@ -23,17 +44,37 @@ const Register = () => {
             <p className="formTitle" >Create a new Account</p>
           <Form.Group>
           <Form.Label>User Name</Form.Label>
-            <Form.Control size="sm" type="text" placeholder="" />
+            <Form.Control 
+            size="sm" 
+            type="text" 
+            placeholder="" 
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            />
           <Form.Label>Your email</Form.Label>
-            <Form.Control size="sm" type="email" placeholder="" />
+            <Form.Control 
+            size="sm" 
+            type="email" 
+            placeholder="" 
+            value={email} 
+            onChange={(e) => setEmail(e.target.value)}
+            />
             <Form.Label>Password</Form.Label>
-            <Form.Control size="sm" type="password" placeholder="" />
+            <Form.Control 
+            size="sm" 
+            type="password" 
+            placeholder="" 
+            value={password}
+            onChange={(e) => setPassword(e.currentTarget.value)}
+            />
           </Form.Group>
+
+          <button className="registerButton"
+        onClick={login}>  Login  </button>
   
           <button className="googleButton">
               <img
-              src={google}
-              />
+              src={google} />
               Sign in with Google
               </button>
             
